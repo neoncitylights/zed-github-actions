673e7b1697d331edcfc2ee4bdf6a09242f3dcd57
Samantha <contact@samanthanguyen.me>
2025-09-25 19:26:40 -0500

    chore(deps): update zed-extension-api to v0.7.0 (#8)

--- src/gh-actions-server.rs
+++ src/gh-actions-server.rs
@@ -10,7 +10,8 @@ struct GitHubActionsExtension {
 
 impl GitHubActionsExtension {
 	fn binary_path() -> String {
-		zed_ext::sanitize_windows_path(std::env::current_dir().unwrap())
+		std::env::current_dir()
+			.unwrap()
 			.join("node_modules")
 			.join(PACKAGE_NAME)
 			.join(format!("bin/{BINARY_NAME}"))
@@ -88,25 +89,3 @@ impl Extension for GitHubActionsExtension {
 }
 
 register_extension!(GitHubActionsExtension);
-
-/// Extensions to the Zed extension API that have not yet stabilized.
-mod zed_ext {
-	/// Sanitizes the given path to remove the leading `/` on Windows.
-	///
-	/// On macOS and Linux this is a no-op.
-	///
-	/// This is a workaround for https://github.com/bytecodealliance/wasmtime/issues/10415.
-	pub fn sanitize_windows_path(path: std::path::PathBuf) -> std::path::PathBuf {
-		use zed_extension_api::{Os, current_platform};
-
-		let (os, _arch) = current_platform();
-		match os {
-			Os::Mac | Os::Linux => path,
-			Os::Windows => path
-				.to_string_lossy()
-				.to_string()
-				.trim_start_matches('/')
-				.into(),
-		}
-	}
-}
